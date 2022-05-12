1b41ee935fca2720a47ed99950b29a17a2a89ac5
eLuizaTsuda <eLuizaTsuda@gmail.com>
2022-05-12 17:28:34 -0230

    problem description

--- w6_assess_word_search_game_driver.py
+++ w6_assess_word_search_game_driver.py
@@ -1,3 +1,12 @@
+# The object of the game is for the players to view the board and find words (remember that the words list is
+# unknown to the players). Words may be contained in rows (from left to right) or columns (from top to bottom),
+# but not backwards. When a player correctly guesses a word that occurs in the words list, that player is 
+# awarded points according to a scoring system described in the starter code. The game ends when all words on
+# the board that appear in the words list have been guessed.
+# The player with the highest score wins.
+# The words from the words list and the letters of the board are made up of alphabetic, uppercase characters.
+
+
 from tkinter.filedialog import askopenfile
 import w6_assess_word_search_game
 
